934d1cdebdeffc66ab769c428d2375ad02068624
ray9510 <liu9510@outlook.com>
2020-05-03 00:56:29 +0800

    v1.1

--- mindustry-server/Form1.Designer.vb
+++ mindustry-server/Form1.Designer.vb
@@ -22,10 +22,67 @@ Partial Class Form1
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "服务器初始设置"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(3, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label3.Location = New System.Drawing.Point(154, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 12)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "ver.1.1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label2.Location = New System.Drawing.Point(12, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 12)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "design by ray1995"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(234, 115)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Mindustry-server"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- mindustry-server/Form1.vb
+++ mindustry-server/Form1.vb
@@ -1,3 +1,64 @@
-﻿Public Class Form1
+﻿Imports System.Net.Sockets
+Imports System.Net
+Imports System.Text
+Imports System.Threading
+Public Class Form1
+    Dim ReceiveThread As New Thread(AddressOf listen, 0)
+    Dim t2 = 0
+    Dim udpx = 1
+    Sub listen()
 
+        Dim udp As New UdpClient(6655)
+
+        Dim RemoteIpEndPoint As New IPEndPoint(IPAddress.Any, 0)
+        Do While udpx = 1
+            Dim receiveBytes As [Byte]() = udp.Receive(RemoteIpEndPoint)
+            Dim returnData As String = Encoding.ASCII.GetString(receiveBytes)
+            BeginInvoke(New EventHandler(AddressOf send), returnData)
+        Loop
+
+    End Sub
+
+    Private Sub send(sender As Object, e As EventArgs)
+        TextBox1.Text = sender
+        Try
+            Dim bytes(1024) As Byte
+            Dim s = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+            Dim localEndPoint As New IPEndPoint(IPAddress.Parse("127.0.0.1"), 6859)
+            s.Connect(localEndPoint)
+            s.Send(Encoding.ASCII.GetBytes(sender))
+            s.Close()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReceiveThread.Start()
+    End Sub
+    Private Sub Form1_close(sender As Object, e As EventArgs) Handles MyBase.Closed
+        Try
+            ReceiveThread.Abort()
+        Catch
+        End Try
+        udpx = 0
+        Dim udpClient As New UdpClient()
+        Dim ipAddress As IPAddress = Dns.Resolve("127.0.0.1").AddressList(0)
+        Dim ipEndPoint As New IPEndPoint(ipAddress, 6655)
+        Dim sendBytes As [Byte]() = Encoding.ASCII.GetBytes("Is anybody there?")
+        udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AppActivate("C:\Windows\System32\cmd.exe")
+        Thread.Sleep(100)
+        SendKeys.Send("config socketInput true")
+        Thread.Sleep(100)
+        SendKeys.Send("{enter}")
+        Thread.Sleep(100)
+        SendKeys.Send("config socketInputPort 6859")
+        Thread.Sleep(100)
+        SendKeys.Send("{enter}")
+
+    End Sub
 End Class
