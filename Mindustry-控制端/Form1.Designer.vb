5e0fbd4e1b7f1379b1125a1c44e3e3205ed67604
ray9510 <liu9510@outlook.com>
2020-05-03 01:00:16 +0800

    控制端

---
+++ Mindustry-控制端/Form1.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Timers.Timer()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.Timer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(131, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Mindustry服务器控制端"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Button7)
+        Me.GroupBox1.Controls.Add(Me.Button6)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 34)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(179, 180)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "快捷指令"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 20)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "启动服务器"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(87, 20)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "停止服务器"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(6, 49)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "结束本局"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(87, 49)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "资源填满"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(6, 88)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(61, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "设置管理"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(6, 146)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(61, 23)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "读取游戏"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(6, 117)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(61, 23)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "储存游戏"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(73, 90)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(89, 21)
+        Me.TextBox1.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(73, 117)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(89, 21)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(73, 146)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(89, 21)
+        Me.TextBox3.TabIndex = 9
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(14, 251)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(111, 23)
+        Me.Button8.TabIndex = 10
+        Me.Button8.Text = "自定义指令-发送"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(14, 224)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(178, 21)
+        Me.TextBox4.TabIndex = 10
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000.0R
+        Me.Timer1.SynchronizingObject = Me
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label2.Location = New System.Drawing.Point(10, 294)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 12)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "design by ray1995"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label3.Location = New System.Drawing.Point(152, 294)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 12)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "ver.1.1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(211, 315)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "控制端"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.Timer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Timer1 As Timers.Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Mindustry-控制端/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.Net.Sockets
+Imports System.Net
+Imports System.Text
+
+Public Class Form1
+    Dim udpClient As New UdpClient()
+    Dim ipAddress As IPAddress = Dns.Resolve(My.Settings.ip1).AddressList(0)
+    Dim ipEndPoint As New IPEndPoint(ipAddress, 6655)
+    Dim sendBytes As [Byte]() = Encoding.ASCII.GetBytes("Is anybody there?")
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sendBytes = Encoding.ASCII.GetBytes("host")
+        udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        sendBytes = Encoding.ASCII.GetBytes("stop")
+        udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        sendBytes = Encoding.ASCII.GetBytes("gameover")
+        udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        sendBytes = Encoding.ASCII.GetBytes("fillitems")
+        udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        sendBytes = Encoding.ASCII.GetBytes("admin add " + TextBox1.Text)
+        udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        sendBytes = Encoding.ASCII.GetBytes("save " + TextBox2.Text)
+        udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        sendBytes = Encoding.ASCII.GetBytes("load " + TextBox2.Text)
+        udpClient.Send(sendBytes, sendBytes.Length, ipEndPoint)
+    End Sub
+    Dim a = 0
+    Dim a1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        a += 1
+        If a > 3 Then
+            a = 0
+            Dim x = InputBox("现在地址:" + My.Settings.ip1, "设置服务器地址", "106.52.195.218",,)
+            If x = "" Then
+                Exit Sub
+            End If
+            '检查IP地址是否合法函数
+            Dim boolIsIP, intLoop As Integer
+            Dim arrIP
+            boolIsIP = 0  '函数初始值为true  
+            arrIP = Split(x, ".") '将输入的IP用"."分割为数组，数组下标从0开始，所以有效IP分割后的数组上界必须为3  
+
+            If UBound(arrIP) <> 3 Then
+                MsgBox("ip错误:" + x)
+                boolIsIP = 1
+            Else
+                For intLoop = 0 To UBound(arrIP)
+                    If Not IsNumeric(arrIP(intLoop)) Then       '检查数组元素中各项是否为数字，如果不是则不是有效IP  
+                        MsgBox("ip错误:" + x)
+                        boolIsIP = 1
+                    Else
+                        If arrIP(intLoop) > 255 Or arrIP(intLoop) < 0 Then       '检查IP数字是否满足IP的取值范围  
+                            MsgBox("ip错误:" + x)
+                            boolIsIP = 1
+                        End If
+                    End If
+                Next
+            End If
+            If boolIsIP = 0 Then
+                My.Settings.ip1 = x
+                Application.Restart()
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Elapsed(sender As Object, e As Timers.ElapsedEventArgs) Handles Timer1.Elapsed
+        If a = a1 Then
+            a = 0
+        ElseIf Not a = 0 Then
+            a1 = a
+
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
